0167655b2512711960748cb0d7514abbc02a5312
智营 <porsche@tencent.com>
2016-09-20 15:33:58 +0800

    增加历史版本归档

---
+++ MSDKDns_Framework/MSDKDns_IOS_1.0.7.3i/MSDKDns/MSDKDns.framework/Headers/MSDKDns.h
@@ -0,0 +1,54 @@
+//
+//  MSDKDns.h
+//  MSDKDns
+//
+//  Created by Mike on 6/24/15.
+//  Copyright (c) 2015 Tencent. All rights reserved.
+//
+#ifndef __MSDKDns_H__
+#define __MSDKDns_H__
+
+#define MSDKDns_Version @"1.0.7.3i"
+
+#import <Foundation/Foundation.h>
+#include "MSDKDnsObserver.h"
+
+#if defined(__has_include)
+#if __has_include("MSDKFoundation/MSDKFoundation.h")
+#include "MSDKFoundation/MSDKFoundation.h"
+#endif
+
+#if __has_include("Analytics/AnalyticsInterface.h")
+#include "Analytics/AnalyticsInterface.h"
+#endif
+
+#if __has_include("BeaconAPI_Base/BeaconBaseInterface.h")
+#include "BeaconAPI_Base/BeaconBaseInterface.h"
+#endif
+#endif
+
+class MSDKDns{
+private:
+    static MSDKDns * m_pInst;
+    
+    MSDKDns();
+    virtual ~MSDKDns();
+    
+public:
+    static MSDKDns* GetInstance();
+    /**
+     *
+     *  @param domain 域名
+     *  @return 查询到的IP数组，超时（3s）或者未未查询到返回空数组
+     */
+    std::vector<unsigned char*> WGGetHostByName(unsigned char* domain);
+    
+    /**
+     *  Log开关
+     *  @param enabled true:打开 false:关闭
+     */
+    void WGOpenMSDKDnsLog(bool enabled);
+    
+};
+
+#endif
