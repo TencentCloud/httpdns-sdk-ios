7e5d2c81de73e983f55a8528d64ee2bef0eab275
智营 <porsche@tencent.com>
2018-11-15 12:17:09 +0800

    更新灯塔framework和dns framework

--- HTTPDNSLibs/MSDKDns/MSDKDns.framework/Headers/MSDKDns.h
+++ HTTPDNSLibs/MSDKDns/MSDKDns.framework/Headers/MSDKDns.h
@@ -8,7 +8,7 @@
 #ifndef __MSDKDns_H__
 #define __MSDKDns_H__
 
-#define MSDKDns_Version @"1.1.1.1i"
+#define MSDKDns_Version @"1.1.2i"
 
 #import <Foundation/Foundation.h>
 #if defined(__has_include)
