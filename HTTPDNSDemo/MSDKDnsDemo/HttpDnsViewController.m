1a64981f439ee9c1ec33ebc499f70f677527b58a
vastly <vastlh@qq.com>
2021-12-24 16:00:00 +0800

    feat: sdk 版本升级到1.3.0

--- HTTPDNSDemo/MSDKDnsDemo/HttpDnsViewController.m
+++ HTTPDNSDemo/MSDKDnsDemo/HttpDnsViewController.m
@@ -61,7 +61,7 @@
         NSTimeInterval time1 = [[NSDate date] timeIntervalSince1970];
         NSArray* result = [[MSDKDns sharedInstance] WGGetHostByName:domains[0]];
         NSTimeInterval time2 = [[NSDate date] timeIntervalSince1970];
-        NSLog(@"本次耗时：%fms", (time2 - time1) * 1000);
+        NSLog(@"=====本次耗时=====：%fms", (time2 - time1) * 1000);
         if (result && result.count > 1) {
             NSString* str = [NSString stringWithFormat:@"\n解析结果为：\nIPV4地址为：%@\nIPV6地址为：%@\n", result[0], result[1]];
             [_resultTextView insertText:str];
@@ -74,7 +74,7 @@
         NSTimeInterval time1 = [[NSDate date] timeIntervalSince1970];
         NSDictionary* result = [[MSDKDns sharedInstance] WGGetHostsByNames:domains];
         NSTimeInterval time2 = [[NSDate date] timeIntervalSince1970];
-        NSLog(@"本次耗时：%fms", (time2 - time1) * 1000);
+        NSLog(@"=====本次耗时=====：%fms", (time2 - time1) * 1000);
         if (result && [result count] > 0) {
             NSString* str = [NSString stringWithFormat:@"\n解析结果为：\n %@\n",result];
             [_resultTextView insertText:str];
@@ -94,7 +94,7 @@
         NSTimeInterval time1 = [[NSDate date] timeIntervalSince1970];
         [[MSDKDns sharedInstance] WGGetHostByNameAsync:domains[0] returnIps:^(NSArray *ipsArray) {
             NSTimeInterval time2 = [[NSDate date] timeIntervalSince1970];
-            NSLog(@"本次耗时：%fms", (time2 - time1) * 1000);
+            NSLog(@"=====本次耗时=====：%fms", (time2 - time1) * 1000);
             if (ipsArray && ipsArray.count > 1) {
                 NSString* str = [NSString stringWithFormat:@"\n解析结果为 \nIPV4地址为：%@\nIPV6地址为：%@\n", ipsArray[0], ipsArray[1]];
                 dispatch_async(dispatch_get_main_queue(), ^{
@@ -112,7 +112,7 @@
         NSTimeInterval time1 = [[NSDate date] timeIntervalSince1970];
         [[MSDKDns sharedInstance] WGGetHostsByNamesAsync:domains returnIps:^(NSDictionary *ipsDict) {
             NSTimeInterval time2 = [[NSDate date] timeIntervalSince1970];
-            NSLog(@"本次耗时：%fms", (time2 - time1) * 1000);
+            NSLog(@"=====本次耗时=====：%fms", (time2 - time1) * 1000);
             if (ipsDict && [ipsDict count] > 0) {
                 NSString* str = [NSString stringWithFormat:@"\n解析结果为：\n：%@\n",ipsDict];
                 dispatch_async(dispatch_get_main_queue(), ^{
